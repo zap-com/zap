847af07fa5837c43e9f020319773234826012e9a
Valerio Oz <valerio.oz@outlook.com>
2020-10-30 22:29:33 +0100

    Minor fixes on nav, routes

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -20,6 +20,21 @@
                         class="btn b-btn">Login/Registrati</button></a>
             @else
                 <!-- User Dropdown -->
+                <div class="dropdown">
+                    <a id="navbarDropdown" class="d-none d-md-block" href="#" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false" v-pre><button id="loginBtn"
+                            class="btn b-btn  dropdown-toggle">{{ Auth::user()->name }}</button></a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </div>
             @endguest
         </div>
     </div>
