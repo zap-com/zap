87be4186c8eebe6aa2fe9632d7ff878d3b39c353
Salvatore Boukaleb <seboukaleb@gmail.com>
2020-10-30 19:46:41 +0100

    category from json, redirect after login, homeController in landingpage

--- database/migrations/2020_10_29_163450_create_categories_table.php
+++ database/migrations/2020_10_29_163450_create_categories_table.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Models\Category;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateCategoriesTable extends Migration
 {
@@ -17,22 +18,22 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            //$table->string('icon');
+            $table->string('icon');
             $table->timestamps();
         });
 
 
-        //prendere da json
-        $categories = [
-
-            'Auto', 'Case', 'Musica', 'Giardinaggio', 'Hobby', 'Cucina', 'Sport'
-
-        ];
+        //prendo le categorie dal json il storage
+        $file = Storage::get('category.json');
+        $categories = json_decode($file);
+   
+     
 
         foreach ($categories as $cat) {
 
             $c = new Category();
-            $c->name = $cat;
+            $c->name = $cat->name;
+            $c->icon = $cat->icon;
             $c->save();
         }
     }
