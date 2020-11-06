631de69bc0f0967cedf94c115f18c4f7ae16fdaf
Valerio Oz <valerio.oz@outlook.com>
2020-11-06 01:22:24 +0100

    Fix: git fuckup

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -14,17 +14,10 @@ class RevisorController extends Controller
 
     public function index()
     {
-<<<<<<< HEAD
-        $announcements = Announcement::where('status_id', 1)
-                        ->orderBy('created_at', 'desc')
-                        ->get();
-        return view('revisor.home', compact('announcements'));
-=======
         $announcement = Announcement::where('status_id', 1)
             ->orderBy('created_at', 'desc')
             ->paginate(16);
         return view('revisor.home', compact('announcement'));
->>>>>>> 405cb77c1c4c1540ecc991e98e1af5e90c6fc791
     }
 
     public function setAccepted(Announcement $announcement)
