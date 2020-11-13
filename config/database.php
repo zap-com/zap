3642ef05360350e634388f9c974f0fbb613bd0c9
Valerio Oz <valerio.oz@outlook.com>
2020-11-13 09:03:14 +0100

    Database rollback and reseeding

--- config/database.php
+++ config/database.php
@@ -16,11 +16,6 @@ return [
     */
 
     'default' => env('DB_CONNECTION', 'mysql'),
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL")),
-    $host = $url["host"],
-    $username = $url["user"],
-    $password = $url["pass"],
-    $database = substr($url["path"], 1),
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -36,7 +31,7 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-        'connections' => [
+    'connections' => [
 
         'sqlite' => [
             'driver' => 'sqlite',
@@ -49,11 +44,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => $host,
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -126,7 +121,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
